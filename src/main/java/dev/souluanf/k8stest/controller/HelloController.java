ab866c5a60ef78aebef0102224dd2de0fd9a1b9f
Luan Fernandes <souluanf@icloud.com>
2026-04-17 23:56:43 -0300

    feat: ✨ Injetar secrets do Doppler no pod via envFrom

--- src/main/java/dev/souluanf/k8stest/controller/HelloController.java
+++ src/main/java/dev/souluanf/k8stest/controller/HelloController.java
@@ -1,5 +1,6 @@
 package dev.souluanf.k8stest.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,11 @@ import java.util.Map;
 @RestController
 public class HelloController {
 
+    @Value("${APP_MESSAGE:k8s-test running on k3s}")
+    private String appMessage;
+
     @GetMapping("/hello")
     public Map<String, String> hello() {
-        return Map.of("message", "k8s-test running on k3s");
+        return Map.of("message", appMessage);
     }
 }
